f46961c6208b901ad37e6db5922719e27502a7f0
Nirveek21 <nirveek21@gmail.com>
2021-11-17 00:14:24 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+    
+  <div className="card text-white bg-success mb-3" style={{maxWidth: "20rem"}}>
+  <div className="card-header">Header</div>
+  <div className="card-body">
+    <h4 className="card-title">Success card title</h4>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
